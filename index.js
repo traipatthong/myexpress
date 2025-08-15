cac83b1548915f6cef1f2a327b5e97480bcbbd00
JINHWONIC <137371846+JINHWONIC@users.noreply.github.com>
2025-08-15 15:28:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function handleEvent(event) {
 
   return client.replyMessage(event.replyToken, {
     type: 'text',
-    text: `คุณพิมพ์ว่า: ${event.message.text}`
+    text: `คุณพิมพ์ว่า xxxx: ${event.message.text}`
   });
 }
 
